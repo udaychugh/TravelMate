4a8f6926f4501fee5878e5349b9b69dcfc36912a
udaychugh <udaychugh8@gmail.com>
2025-01-10 23:59:42 +0530

    minor fixes

--- app/src/main/java/com/freelab/tech/travelmate/ui/preauth/PreAuthActivity.kt
+++ app/src/main/java/com/freelab/tech/travelmate/ui/preauth/PreAuthActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.freelab.tech.travelmate.ui.navigation.AppNavigation
 import com.freelab.tech.travelmate.ui.theme.TravelMateTheme
 
 class PreAuthActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
